c19c55aa97b2e7235de32cf891ff904946503f0f
taegeumLee <xorma443101@gmail.com>
2024-11-08 14:33:40 +0900

    create login -ing

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,4 +1,4 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import LoginForm from "@/components/loginForm";
 import Link from "next/link";
 export default function Home() {
   return (
@@ -8,15 +8,26 @@ export default function Home() {
           <span className="text-8xl text-blue-400 ">VIVA</span>
           <h4 className="text-l ">새로운 수업! 재밌는 수업!</h4>
         </div>
+        <LoginForm />
         <div className="flex flex-col items-center gap-2 w-full ">
-          <button className="py-2.5 text-lg w-full text-neutral-900 bg-neutral-100 rounded-lg p-4 hover:bg-blue-400 hover:text-neutral-100 transition-all">
+          <button className="py-2.5 mb-2 text-lg w-full text-neutral-900 bg-neutral-100 rounded-lg p-4 hover:bg-blue-400 hover:text-neutral-100 transition-all">
             구글 로그인
           </button>
-          <button className="py-2.5 text-lg w-full my-2 text-neutral-900 bg-neutral-100 rounded-lg p-4 hover:bg-blue-400 hover:text-neutral-100 transition-all">
-            이메일 로그인
+          <button className="py-2.5 mb-2 text-lg w-full text-neutral-900 bg-neutral-100 rounded-lg p-4 hover:bg-green-400 hover:text-neutral-100 transition-all">
+            네이버 로그인
           </button>
+          <button className="py-2.5 mb-2 text-lg w-full text-neutral-900 bg-neutral-100 rounded-lg p-4 hover:bg-yellow-400 hover:text-neutral-100 transition-all">
+            카카오 로그인
+          </button>
+
           <div className="w-3/4 h-px bg-neutral-700"></div>
 
+          <Link
+            href="/create-account"
+            className="py-2.5 mb-2  text-lg w-full text-neutral-900 bg-neutral-100 rounded-lg p-4 hover:bg-blue-400 hover:text-neutral-100 transition-all"
+          >
+            이메일로 가입하기
+          </Link>
           <div className="flex flex-row gap-2  justify-center items-center ">
             <span>이미 계정이 있으신가요?</span>
             <Link
---
+++ components/loginForm.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+export default function LoginForm() {
+  return (
+    <form action="" className="flex flex-col gap-2 mx-auto">
+      <div className="flex flex-row">
+        <div className="w-1/2">
+          <input
+            className="bg-neutral-100 p-2 rounded-lg"
+            type="text"
+            placeholder="아이디를 입력하세요"
+          />
+          <input
+            className="bg-neutral-100 p-2 rounded-lg"
+            type="password"
+            placeholder="비밀번호를 입력하세요"
+          />
+        </div>
+        <div className="w-1/2">
+          <button className="bg-blue-400 text-neutral-100 p-2 rounded-lg">
+            로그인
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
